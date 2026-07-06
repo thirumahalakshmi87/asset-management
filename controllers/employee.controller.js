a5207882daed11e56c5d27d41eb7e086d3321eff
thirumahalakshmi87 <thiru.mahalakshmi87@gmail.com>
2026-07-07 05:25:06 +0530

    Employee Branch and Department Addition

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,11 +1,36 @@
 const { Op } = require('sequelize');
 const Employee = require('../models/employee');
+const Branch = require('../models/branch');
+const Department = require('../models/department');
+
+Employee.belongsTo(Branch, {
+    foreignKey: 'branch_id',
+    as: 'branch'
+});
+
+Employee.belongsTo(Department, {
+    foreignKey: 'department_id',
+    as: 'department'
+});
+
 
 exports.getAll = async (req, res) => {
     try {
         const employees = await Employee.findAll({
-            order: [['created_at', 'DESC']]
-        });
+			include: [
+				{
+					model: Branch,
+					as: 'branch',
+					attributes: ['branch_name']
+				},
+				{
+					model: Department,
+					as: 'department',
+					attributes: ['department_name']
+				}
+			],
+			order: [['created_at', 'DESC']]
+		});
         res.render('employee/view', {
             title: 'Employee Master',
             employees
@@ -16,16 +41,24 @@ exports.getAll = async (req, res) => {
     }
 };
 
-exports.addForm = (req, res) => {
+exports.addForm = async (req, res) => {
+	const branches = await Branch.findAll({
+        where: { is_active: true }
+    });
+    const departments = await Department.findAll({
+        where: { is_active: true }
+    });	
     res.render('employee/add', {
-        title: 'Add Employee'
+        title: 'Add Employee', 
+		branches,
+        departments
     });
 };
 
 exports.add = async (req, res) => {
     try {
-        const { employee_code, first_name, last_name, email, mobile, designation, status } = req.body;
-        await Employee.create({ employee_code, first_name, last_name, email: email || null, mobile, designation, status: status === 'true' });
+        const { employee_code, first_name, last_name, email, mobile, branch_id, department_id, designation, status } = req.body;
+        await Employee.create({ employee_code, first_name, last_name, email: email || null, mobile, designation, branch_id, department_id, status: status === 'true' });
         req.flash('success', 'Employee added successfully');
         res.redirect('/employees');
     } catch (error) {
@@ -57,9 +90,17 @@ exports.editForm = async (req, res) => {
         if (!employee) {
             return res.status(404).send('Employee not found');
         }
+		const branches = await Branch.findAll({
+        where: { is_active: true }
+		});
+		const departments = await Department.findAll({
+			where: { is_active: true }
+		});
         res.render('employee/edit', {
             title: 'Edit Employee',
-            employee
+            employee,
+            branches,
+            departments
         });
     } catch (error) {
         console.error(error);
@@ -73,8 +114,8 @@ exports.update = async (req, res) => {
         if (!employee) {
             return res.status(404).send('Employee not found');
         }
-        const { first_name, last_name, email, mobile, designation, status } = req.body;
-        await employee.update({ first_name, last_name, email: email || null, mobile, designation, status: status === 'true' });
+        const { first_name, last_name, email, mobile, branch_id, department_id, designation, status } = req.body;
+        await employee.update({ first_name, last_name, email: email || null, mobile, branch_id, department_id, designation, status: status === 'true' });
         req.flash('success', 'Employee updated successfully');
         res.redirect('/employees');
     } catch (error) {
